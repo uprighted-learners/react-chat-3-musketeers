d506959ccdc1b22b4efe8954c8f6351d851f005c
[Tiffania] <ielenatiffania@gmail.com>
2023-07-03 15:25:29 -0400

    Endpoint create for rooms

--- server/controllers/rooms.js
+++ server/controllers/rooms.js
@@ -1,4 +1,23 @@
 const express = require("express")
 const router = express.Router()
+const Room = require("../models/room");
+
+
+router.post("/create", async (req, res) => {
+    try {
+        const { name, description, addedUsers } = req.body
+        const newRoom = new Room(req.body)
+        const saver = await newRoom.save()
+        res.status(201).json({
+            message: `Room created`,
+            newRoom
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: `${err}`
+        })
+    }
+})
+
 
 module.exports = router
